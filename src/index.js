cb27ba77b8f4329e3010c0b3d14747ceacaf5639
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-09-07 00:38:29 +0200

    subsequent updates and commits

--- src/index.js
+++ src/index.js
@@ -8,11 +8,24 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+//dev-eu6divs6.us.auth0.com
+//mXgYzaUQ1d1Tzm2oxQVEoiowfrGV5G1Q
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-eu6divs6.us.auth0.com'
+    clientId='mXgYzaUQ1d1Tzm2oxQVEoiowfrGV5G1Q'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 )
