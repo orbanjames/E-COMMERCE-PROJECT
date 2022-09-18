f5ca2c7b9a8cf5c8d8570d7bfea1f4898b3b014b
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-09-18 02:02:59 +0200

    subsequent updates and commits

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FeaturedProducts, Hero, Services, Contact } from '../components'
+//import Slide from '../components/slide'
+//import '../components/slide.css'
 const HomePage = () => {
   return (
     <main>
